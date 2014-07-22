f2ef7f5611739fb8f07a71901017a611e0e90b0c
SavinaRoja <pablo.barton@gmail.com>
2014-07-22 13:08:02 -0400

    fixed some totally wack code, I'm pretty sure it didn't work before

--- npyscreen/pmfuncs.py
+++ npyscreen/pmfuncs.py
@@ -1,34 +1,40 @@
-#!/usr/bin/python
+# encoding: utf-8
 
 import curses
+import os
+
 
 class ResizeError(Exception):
     "The screen has been resized"
-    
-def hidecursor():
+
+
+def hide_cursor():
     try:
         curses.curs_set(0)
     except:
         pass
 
-def showcursor():
+
+def show_cursor():
     try:
         curses.curs_set(1)
     except:
         pass
 
-def CallSubShell(subshell):
-    """Call this function if you need to execute an external command in a subshell (os.system).  All the usual warnings apply -- the command line will be
-    expanded by the shell, so make sure it is safe before passing it to this function."""
+
+def call_subshell(subshell):
+    """
+    Call this function if you need to execute an external command in a subshell
+    (os.system).  All the usual warnings apply -- the command line will be
+    expanded by the shell, so make sure it is safe before passing it to this
+    function.
+    """
     curses.def_prog_mode()
     #curses.endwin() # Probably causes a memory leak.
-    
+
     rtn = os.system("%s" % (subshell))
     curses.reset_prog_mode()
-    if rtn is not 0: return False
-    else: return True
-
-    curses.reset_prog_mode()
-
-hide_cursor = hidecursor
-show_cursor = showcursor
+    if rtn is not 0:
+        return False
+    else:
+        return True
